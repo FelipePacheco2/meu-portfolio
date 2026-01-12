f759e9b59ef5efa8b6c12ed71a741a45f497e8d7
FelipePacheco <felipecaballeropacheco@gmail.com>
2026-01-12 03:58:43 +0000

    Removendo a pasta ComandaHub

--- ComandaHub/projetecs-lab-vet/src/Servico/AbsServico.java
+++
@@ -1,7 +0,0 @@
-package Servico;
-
-import Repositorio.AbsRepositorio;
-
-public abstract class AbsServico<TDominio>{
-    protected AbsRepositorio<TDominio> repositorio;
-}
--- ComandaHub/src/main/java/br/com/felipe/Treinamento/exceptionReponse/ExceptionReponse.java
+++
@@ -1,7 +0,0 @@
-package br.com.felipe.Treinamento.exceptionReponse;
-
-import java.util.Date;
-
-public record ExceptionReponse(Date time, String msg, String dails) {
-
-}
